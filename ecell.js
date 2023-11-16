210d798c735ed3cbbd05041af5f99936801f1b20
medha-14 <143182673+medha-14@users.noreply.github.com>
2023-11-16 23:10:49 +0530

    Add files via upload

---
+++ ecell.js
@@ -0,0 +1,9 @@
+
+
+function openNav() {
+  document.getElementById("myNav").style.width = "35%"; /** from 100% to 25% **/
+}
+
+function closeNav() {
+  document.getElementById("myNav").style.width = "0";
+}
